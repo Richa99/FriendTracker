27be61dd39005d3aefcf12faa78e6d3ba7ba8886
Richa99 <49717733+Richa99@users.noreply.github.com>
2019-04-19 12:58:12 +0530

    Update MapsActivity.java

--- app/src/main/java/com/example/administration/friendtracker/MapsActivity.java
+++ app/src/main/java/com/example/administration/friendtracker/MapsActivity.java
@@ -115,24 +115,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+   
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-       /* LatLng sydney = new LatLng(-31, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-
 
     }
     public void ChangeType(View view)
@@ -160,14 +146,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         startActivity(i1);
 
     }
-    /* @Override
-     public boolean onCreateOptionsMenu(Menu menu)
-     {
-         MenuInflater inflater= getMenuInflater();
-         inflater.inflate(R.menu.main_menu,menu);
-         return true;
-     }
- */
+   
     @Override
     protected void onStop() {
         super.onStop();
